06f51d3af40c2013202aa3ae0df09c685582a831
John Murray <jbm1991@gmail.com>
2025-03-17 22:17:00 +0000

    add mongoose for task management and implement basic CRUD routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,36 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import taskRoutes from "./routes/taskRoutes";
 
 dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 const PORT = process.env.PORT || 4444;
+const MONGO_URI = process.env.MONGO_URI as string;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+// Connect to MongoDB
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB: ", error.message);
+  });
+
 // Health check route
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Server is running" });
 });
+
+// Task routes
+app.use("/api/tasks", taskRoutes);
 
 // Start server
 app.listen(PORT, () => {
